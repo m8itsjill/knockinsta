6a90cd17ff7e9110e21736cabc9d0302359585ad
haileysmallwood <hpkpixie@bellsouth.net>
2019-07-15 16:07:22 -0700

    first save

---
+++ knockInsta/AddPhotoViewController.swift
@@ -0,0 +1,38 @@
+//
+//  AddPhotoViewController.swift
+//  knockInsta
+//
+//  Created by Apple on 7/15/19.
+//  Copyright © 2019 HS. All rights reserved.
+//
+
+import UIKit
+
+class AddPhotoViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    var imagePicker = UIImagePickerController()
+    
+    
+    @IBAction func savebutton(_ sender: Any) {
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
